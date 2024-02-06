dbbbb8b5c690a84c5e7b909ec54b317a5cd5d44b
Diogo <alvarwngadiogo228@gmail.com>
2024-02-06 16:31:32 -0300

    ultimo

--- desafio2/index.js
+++ desafio2/index.js
@@ -1,9 +1,19 @@
-let productValue = Number(prompt('Enter the product price: ',))// input
+let productValue = Number(prompt('Enter the product price: '))// input
+
+
+
+
 if (productValue >= 20){
     document.getElementById('result').innerHTML = 'Approved'//linkar a variavel do html
-
+    document.getElementById('purchase').innerHTML = productValue
+    
 }
+
+    
 
 else{
     document.getElementById('result').innerHTML = 'Denied'//linkar novamente
+    document.getElementById('purchase').innerHTML = productValue
+    document.getElementById('missing').innerHTML = 20 - productValue
+    
 }
